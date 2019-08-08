3e99d134a6feac7b2de67d356b965448b5c1fbbd
Han Myeong-Ho <thks1018@gmail.com>
2019-08-08 11:21:15 +0900

    Upload blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,16 @@
+PLUGIN.name = "Blacklist"
+PLUGIN.author = "Dobytchick, Frosty"
+PLUGIN.description = "This plugin dds a blacklist. Do not remove those who are in it, because they can harm your servers"
+
+local blacklist =
+{
+-- ["STEAM_0:0:000000000"] = {reason =     ""},
+}
+if SERVER then
+	function PLUGIN:PlayerAuthed(client, steamid, uniqueid)
+		if blacklist[client:SteamID()] then
+			client:Kick("You are on the blackist of the server, reason:" ..blacklist[client:SteamID()].reason)
+			lient:Ban(0, false)
+		end
+	end
+end
